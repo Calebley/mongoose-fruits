8c6229cd53852181b2b1003e193bcd49a778156c
Caleb <calebley@hotmail.com>
2022-03-31 19:23:49 +0800

    done

---
+++ readme.md
@@ -0,0 +1,15 @@
+# 7 REST Routes for fruits
+
+* New - GET - /fruits/new  (form)
+* Create - POST - /fruits/ (do Create)
+* Index - GET - /fruits (All fruits)
+* Show - GET - /fruits/:id 
+
+* Delete - DELETE  - /fruits/:id 
+* Update - 
+
+## Fruit
+
+- name
+- colour
+- readyToEat
---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const log = require("debug")("fruits:server");
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const methodOverride = require("method-override")
+const fruitsController = require("./controllers/fruits.js");
+
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, {}, () => {
+    log("connected to mongodb");
+});
+const app = express();
+const PORT = process.env.PORT ?? 2000;
+
+app.use(morgan("tiny"));
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: false }));
+app.use("/fruits", fruitsController);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+
+app.listen(PORT, () => {
+  log("express started on " + PORT);
+});
